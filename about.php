bf0ffe6ec23e52742239bc7c117e0440bcb4037b
Komalrk143 <88570007+Komalrk143@users.noreply.github.com>
2021-08-07 12:56:05 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About us | Piggy Bank</title>
+    <link rel="shortcut icon" type="image/png" href="icon.png">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    
+</head>
+<body>
+    <section id="aboutus">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-7">
+                    <h1><ins>About us</ins></h1>
+                    <p>The concept of saving money is an important life’s lesson, and probably you (and many of the rest of us), began with the humble <strong>‘Piggy Bank’.</strong> We’re all familiar with the concept, and the term ‘piggy bank’, but where did they first come from? And why the reference to pig?</p>
+                    <p>It’s said that piggy banks have been around since almost 600 years ago, from the time of The Middle Ages. Before there were formal banking systems, people used to keep money at home — in a clay jar called a pygg pot, because it was made of pygg, an orange-coloured clay.</p>
+                    <p>Over the next two to three hundred years, the English language evolved, and “pygg” was pronounced as “pig”, and 19th century English potters forgot “pygg” once referred to earthenware pots. When they received requests for pygg banks, they started making these in the shape of pigs instead, to the delight and amusement of customers and children.
+                    Still a great teacher of the old adage “save for a rainy day”, piggy banks have since evolved from even physical forms to digital ones, in the form of regular savings plans. These plans make sure that we put aside an affordable sum of money aside every month to help us save for a rainy day.</p>
+                    <p>It’s like a physical piggy bank—but even better because you won’t have to break it open to get the money you put in, and it even grows your money for you over time.</p>
+                </div>
+                <div class="col-sm-5"></div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,4 @@
+<div id="container">
+    <div id="header">© 2021. Made by Rk <br>
+      @piggybank_OnlineTransaction.com</div>
+</div>
